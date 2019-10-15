b20dd2fe144af6c943d601f692ae32c5a8345b77
Woojung618 <eyes_cat@naver.com>
2019-10-15 15:53:06 +0900

    6_7

--- main.c
+++ main.c
@@ -3,12 +3,28 @@
 #include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int factorial(int n); int combination(int x,int y,int z); 
+
 
 int main(void) {
-	int i; int max=45;
-	srand((unsigned)time(NULL));
-	for (i=0;i<6;i++)
-		printf("%d ",1+rand()%max);
+	int n; int r;int x;int y; int z; int result;
+	printf("n값과 r값을 순서대로 입력하세요:");
+	scanf("%d %d",&n,&r);
+	x=factorial(n);
+	y=factorial(n-r);
+	z=factorial(r);
+	result=combination(x,y,z);
+	printf("C(%d,%d)=%d",n,r,result);
 	return 0;
 }
+int factorial(int n){
+	int res=1;int i;
+	for (i=1;i<=n;i++)
+		res*=i;
+	return res;
+}
+int combination(int x,int y,int z){
+	return x/(y*z);
+}
+
 
