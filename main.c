4b3ca87339966bbe2643dcea0f4bcf0ef440535c
Woojung618 <eyes_cat@naver.com>
2019-10-15 15:18:10 +0900

    06_4

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void square(int a){
-	a=a*a;
-}
-int main(int argc, char *argv[]) {
-	int a=2; 
-	square(a);
-	printf("a=%i\n",a);
+int square(int n);
+int main(void) {
+	int result;
+	result=square(5);
 	
+	printf("%d",result);
+	
+}
+int square(int n){
+	return (n*n);
 }
